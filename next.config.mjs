ff56bc5ef809560d3ac5dc3aef6f6667585fa963
Your Name <you@example.com>
2025-02-21 16:24:43 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true, // Disables type checking on build
+      },
+};
 
 export default nextConfig;
